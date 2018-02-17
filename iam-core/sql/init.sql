baf80271a1d4c5c21388f59c2a8e7cde31802d75
Le roi <claudeforlife@gmail.com>
2018-02-17 22:33:05 +0100

    sql schema for users
    adding sql schema for login  creation

--- iam-core/sql/init.sql
+++ iam-core/sql/init.sql
@@ -7,3 +7,12 @@ CREATE TABLE IDENTITIES
     EMAIL VARCHAR(255),
     UID VARCHAR(255)
     );
+    
+    CREATE TABLE USERS (
+    userid INT NOT NULL GENERATED ALWAYS AS IDENTITY,
+    username VARCHAR(32) NOT NULL,
+    pass_salt VARCHAR(255) NOT NULL,
+            -- a string of 16 random bytes
+    pass_md5 VARCHAR(255) NOT NULL
+            -- binary MD5 hash of pass_salt concatenated with the password
+);
