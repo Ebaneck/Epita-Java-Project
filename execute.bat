d1494e5b19c7664bc32e898abe3b1c7b733df818
Le roi <claudeforlife@gmail.com>
2018-02-19 21:41:25 +0100

    improve the bat file

--- execute.bat
+++ execute.bat
@@ -1,3 +1,31 @@
 @echo OFF
+
+cd "C:\Users\ceban\Desktop\Claude\db-derby-10.14.1.0-bin\db-derby-10.14.1.0-bin\bin"
+
+start cmd /k call startNetworkServer.bat
+
+
+cd "C:\Users\ceban\Desktop\Claude\db-derby-10.14.1.0-bin\db-derby-10.14.1.0-bin\bin"
+
+start cmd /k call ij.bat  
+  
+  
+   ::Create the database and open a connection to the database using 
+
+:: CONNECT 'jdbc:derby://localhost:1527/iam-core;create=true' USER 'root' PASSWORD 'password';
+
+	
+
+   ::  run sql command and import the database schema
+   
+:: run 'C:\Users\ceban\Desktop\Claude\2017s2_fundamental_b\Epita Java Project\iam-core\sql\init.sql'
+
+
 cd "C:\Users\ceban\Desktop\"
 java -jar  -Dconf=testConfiguration.properties iamcore.jar
+
+
+
+
+
+
