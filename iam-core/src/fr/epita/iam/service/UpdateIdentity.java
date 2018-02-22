024ed4775d9d91b50fedb013e83dc910705398e6
Le roi <claudeforlife@gmail.com>
2018-02-22 14:37:21 +0100

    fix

--- iam-core/src/fr/epita/iam/service/UpdateIdentity.java
+++ iam-core/src/fr/epita/iam/service/UpdateIdentity.java
@@ -37,8 +37,7 @@ public class UpdateIdentity {
 		}
 		String identityID = scanner.nextLine();
 		Identity foundIdentity = identityJDBCDAO.locate(identityID);
-		//if (foundIdentity == null && !foundIdentity.equals("")){
-			if (foundIdentity != null){
+		if (foundIdentity != null){
 			System.out.println("Did not find identity "+identityID);
 		}
 		else{
